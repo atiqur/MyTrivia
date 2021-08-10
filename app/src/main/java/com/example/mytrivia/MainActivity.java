1c98d372af26ffa7f1eb47281660dc2f5a6bb7f0
Atiqur Rahman <atiqur.india@gmail.com>
2021-08-10 22:56:29 +0530

    Display score and fix question counter.

--- app/src/main/java/com/example/mytrivia/MainActivity.java
+++ app/src/main/java/com/example/mytrivia/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.mytrivia;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 import com.example.mytrivia.data.Repository;
 import com.example.mytrivia.databinding.ActivityMainBinding;
 import com.example.mytrivia.model.Question;
+import com.example.mytrivia.model.Score;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private int currentQuestionNumber = 0;
     private int currentScore = 0;
+    private Score score;
     private ActivityMainBinding binding;
     List<Question> questionsList;
 
@@ -27,9 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        score = new Score(currentScore);
+
         questionsList = new Repository().getQuestion(questionArrayList -> {
+            updateCounter(questionArrayList);
             binding.questionTextView.setText(questionArrayList.get(currentQuestionNumber).getAnswer());
         });
+
+        updateScore();
 
         binding.nextButton.setOnClickListener(view -> {
             updateQuestion();
@@ -51,21 +58,41 @@ public class MainActivity extends AppCompatActivity {
         int snackMessage;
         if (answer == userResponse) {
             snackMessage = R.string.correct;
+            addPoints();
         } else {
             snackMessage = R.string.incorrect;
+            deductPoints();
         }
         Snackbar.make(binding.questionCard, snackMessage, Snackbar.LENGTH_SHORT).show();
     }
 
-    private void getNextQuestion() {
-        currentQuestionNumber = (currentQuestionNumber + 1) % questionsList.size();
+    private void addPoints() {
+        currentScore += 100;
+        score.setScore(currentScore);
+        updateScore();
+    }
+
+    private void deductPoints() {
+        if (currentScore > 0) {
+            currentScore -= 100;
+            score.setScore(currentScore);
+        }
+        updateScore();
+    }
+
+    private void updateScore() {
+        binding.currentScoreText.setText(MessageFormat.format("Score: {0}", score.getScore()));
     }
 
     private void updateQuestion() {
-        updateCounter();
-        getNextQuestion();
+        currentQuestionNumber = (currentQuestionNumber + 1) % questionsList.size();
 
         String question = questionsList.get(currentQuestionNumber).getAnswer();
         binding.questionTextView.setText(question);
+        updateCounter(questionsList);
+    }
+
+    private void updateCounter(List<Question> questionArrayList) {
+        binding.questionNumberText.setText(MessageFormat.format("{0} / {1}", (currentQuestionNumber+1), questionArrayList.size()));
     }
 }
